d8d56ab26024d29f4ce35a139a9de8a70e77cab8
der mustafa <sihlider27@gmail.com>
2021-08-20 15:36:10 +0300

    kodlarım

---
+++ OOP1/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }   //bu veriyi anlatan ana anahtardır tc no gibi categoryId ise ikinci referans tipidir.
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+        
+        // Bu tip classlarda sadece özellik olur.
+    }
+}
